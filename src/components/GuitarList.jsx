3f9b058d98381be54a883d0144e0150eaa6d0f74
Carlos Gámez <hardpx@gmail.com>
2024-06-18 11:03:01 -0600

    show guitars functionality

--- src/components/GuitarList.jsx
+++ src/components/GuitarList.jsx
@@ -5,9 +5,28 @@ function GuitarList() {
     return state.guitars.data;
   });
 
-  console.log(guitars);
+  const hanldeGuitarDelete = (guitar) => {};
 
-  return <div>GuitarList</div>;
+  const renderedGuitars = guitars.map((guitar) => {
+    return (
+      <div key={guitar.id} className='panel'>
+        <p>
+          {guitar.name} - ${guitar.cost}
+        </p>
+        <button
+          className='button is-danger'
+          onClick={() => hanldeGuitarDelete(guitar)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div className='card-list'>
+      {renderedGuitars} <hr />
+    </div>
+  );
 }
 
 export default GuitarList;
