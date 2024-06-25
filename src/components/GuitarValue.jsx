422e072ddff5cbe3638a4e333e107a0a911da297
Carlos Gámez <hardpx@gmail.com>
2024-06-25 13:25:10 -0600

    added value functionality

--- src/components/GuitarValue.jsx
+++ src/components/GuitarValue.jsx
@@ -1,5 +1,17 @@
+import { useSelector } from 'react-redux';
+
 function GuitarValue() {
-  return <div>GuitarValue</div>;
+  const totalCost = useSelector(({ guitars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((guitar) => {
+      return guitar.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+    return filteredCars.reduce((acc, guitar) => {
+      return acc + guitar.cost;
+    }, 0);
+  });
+
+  return <div className='guitar-value'>Total Cost: ${totalCost}</div>;
 }
 
 export default GuitarValue;
